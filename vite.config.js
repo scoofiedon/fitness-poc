0346e5a1812d7745cd87b93923764f14e9a3b9d3
artied <po4e4k@gmail.com>
2025-10-05 17:57:16 +0300

    another fix  vite.config.json

--- vite.config.js
+++ vite.config.js
@@ -18,28 +18,28 @@
 // })
 
 
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [vue()],
-//   base: '/fitness-poc/', // This should match your repository name
-//   build: {
-//     outDir: 'dist',
-//     assetsDir: 'assets',
-//     sourcemap: false
-//   },
-//   server: {
-//     host: true, // Listen on all addresses
-//     port: 5173
-//   }
-// })
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/fitness-poc/' // 👈 Replace 'fitness-poc' with your actual repo name
+  base: '/scoofiedon/fitness-poc', // This should match your repository name
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  },
+  server: {
+    host: true, // Listen on all addresses
+    port: 5173
+  }
 })
+
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// export default defineConfig({
+//   plugins: [vue()],
+//   base: '/fitness-poc/' // 👈 Replace 'fitness-poc' with your actual repo name
+// })
